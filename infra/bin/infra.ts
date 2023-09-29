0d7bc62fb8da1ed11405de70ddb47f87e6174266
JorgitoR <jorgitouribe133@gmail.com>
2023-09-29 10:07:30 -0500

    feat: adds nodejs lambda package

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -2,7 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Serverless } from '../lib/serverless';
+import { ServerlessNodejs, ServerlessPython } from '../lib/serverless';
 
 const app = new cdk.App();
 const config = app.node.tryGetContext('config')
@@ -10,13 +10,24 @@ const config = app.node.tryGetContext('config')
 switch(config){
 
   case process.env.STACK_CONFIG_LAMBDA:
-    const lambda = new Serverless(app, String(process.env.STACK_NAME), {
-      env: {
-        account: process.env.AWS_ACCOUNT_ID,
-        region: process.env.AWS_REGION
-      }
-    });
-    taggingStack(lambda, 'pc-1', 'hexagonal', 'dev', 'cc-0001203', 'na', 'na')
+    if(process.env.AWS_SDK === 'nodejs'){
+      const lambda = new ServerlessNodejs(app, String(process.env.STACK_NAME), {
+        env: {
+          account: process.env.AWS_ACCOUNT_ID,
+          region: process.env.AWS_REGION
+        }
+      });
+      taggingStack(lambda, 'pc-1', 'hexagonal', 'dev', 'cc-0001203', 'na', 'na')
+    }
+    if(process.env.AWS_SDK === 'python'){
+      const lambda = new ServerlessPython(app, String(process.env.STACK_NAME), {
+        env: {
+          account: process.env.AWS_ACCOUNT_ID,
+          region: process.env.AWS_REGION
+        }
+      });
+      taggingStack(lambda, 'pc-1', 'hexagonal', 'dev', 'cc-0001203', 'na', 'na')
+    }
     break;
 
 }
--- infra/lib/serverless.ts
+++ infra/lib/serverless.ts
@@ -11,7 +11,58 @@ import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { join } from 'path';
 
-export class Serverless extends cdk.Stack {
+export class ServerlessNodejs extends cdk.Stack {
+
+    constructor(app:Construct, id:string, props: cdk.StackProps){
+        super(app, id, props)
+
+        console.log("desplegando la infraestructura: ", process.env.AWS_SDK)
+
+        const role = new Role(this, String(process.env.LAMBDA_NAME)+'-role', {
+            assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+            roleName: `${process.env.LAMBDA_NAME}-role`
+        });
+
+        if(String(process.env.AWS_SK) === 'nodejs'){
+            console.log("Deploying lambda with sdk nodejs")
+            const lambdaNode =new NodejsFunction(this, String(process.env.LAMBDA_NAME)+'-lambda', {
+                entry: join(
+                    __dirname,
+                    "../../back/lambda/example/handler.js"
+                ),
+                depsLockFilePath: join(
+                    __dirname,
+                    "../../back/lambda/example/package-lock.js"
+                ),
+                runtime: Runtime.NODEJS_18_X,
+                timeout: cdk.Duration.minutes(1),
+                role: role,
+                functionName: String(process.env.LAMBDA_NAME),
+                description: String(process.env.LAMBDA_DESCRIPTION),
+                memorySize:256, 
+                environment:{
+
+                }
+            });
+
+            lambdaNode.addToRolePolicy(new PolicyStatement({
+                effect: Effect.ALLOW,
+                resources: ["arn:aws:dynamodb:us-east-1:408058604061:table/payment"],
+                actions: ["dynamodb:*"]
+            }));
+
+            new cdk.CfnOutput(this, String(process.env.LAMBDA_NAME)+'-cnf', {
+                value: lambdaNode.functionName,
+                description: String(process.env.LAMBDA_DESCRIPTION)
+            })
+
+        }   
+    }
+
+}
+
+
+export class ServerlessPython extends cdk.Stack {
 
     constructor(app:Construct, id:string, props: cdk.StackProps){
         super(app, id, props)
@@ -60,41 +111,6 @@ export class Serverless extends cdk.Stack {
                 description: String(process.env.LAMBDA_DESCRIPTION)
             })
         }
-
-        if(String(process.env.AWS_SK) === 'nodejs'){
-            console.log("Deploying lambda with sdk nodejs")
-            const lambdaNode =new NodejsFunction(this, String(process.env.LAMBDA_NAME)+'-lambda', {
-                entry: join(
-                    __dirname,
-                    "../../back/lambda/example/handler.js"
-                ),
-                depsLockFilePath: join(
-                    __dirname,
-                    "../../back/lambda/example/package-lock.js"
-                ),
-                runtime: Runtime.NODEJS_18_X,
-                timeout: cdk.Duration.minutes(1),
-                role: role,
-                functionName: String(process.env.LAMBDA_NAME),
-                description: String(process.env.LAMBDA_DESCRIPTION),
-                memorySize:256, 
-                environment:{
-
-                }
-            });
-
-            lambdaNode.addToRolePolicy(new PolicyStatement({
-                effect: Effect.ALLOW,
-                resources: ["arn:aws:dynamodb:us-east-1:408058604061:table/payment"],
-                actions: ["dynamodb:*"]
-            }));
-
-            new cdk.CfnOutput(this, String(process.env.LAMBDA_NAME)+'-cnf', {
-                value: lambdaNode.functionName,
-                description: String(process.env.LAMBDA_DESCRIPTION)
-            })
-
-        }   
     }
 
 }
